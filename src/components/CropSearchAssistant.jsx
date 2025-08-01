488667fbcd55d6168c51aaaddcfda1d2c808cfbd
Carl Maingi <carlmaingi@gmail.com>
2025-08-01 11:21:44 +0300

    CSS modifications

--- src/components/CropSearchAssistant.jsx
+++ src/components/CropSearchAssistant.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import knowledgeBase from '../data/knowledgebase.json';
+import styles from './CropSearchAssistant.module.css';
 
 export default function CropSearchAssistant() {
   const [query, setQuery] = useState('');
@@ -13,21 +14,20 @@ export default function CropSearchAssistant() {
   }, [query]);
 
   return (
-    <div>
+    <div className={styles.container}>
       <input
         type="text"
         placeholder="Search Crops, Fertilizers, Guides..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className={styles.inputField}
       />
-      <ul>
-        {filteredResources.map((resource, index) => (
-          <li key={index}>
-            <strong>{resource.name}</strong>
-            <p>{resource.info}</p>
-          </li>
-        ))}
-      </ul>
+      {filteredResources.map((resource, index) => (
+        <div key={index} className={styles.resultCard}>
+          <div className={styles.resultTitle}>{resource.name}</div>
+          <div className={styles.resultInfo}>{resource.info}</div>
+        </div>
+      ))}
     </div>
   );
 }
